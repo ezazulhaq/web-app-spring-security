aa2f073c964da27e993e04ed73e6cca85ba17b83
ezazulhaq <61641801+ezazulhaq@users.noreply.github.com>
2022-02-20 15:52:58 +0530

    first commit for spring security demo

---
+++ src/main/java/com/haa/webappspringsecurity/WebAppSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.haa.webappspringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAppSpringSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/haa/webappspringsecurity/WebAppSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haa.webappspringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAppSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
