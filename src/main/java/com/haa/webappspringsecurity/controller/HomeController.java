8e7e2ccf9fb82eccc022ed80ffb2624424193051
ezazulhaq <61641801+ezazulhaq@users.noreply.github.com>
2022-02-21 16:38:28 +0530

    home page added after login is successful

---
+++ src/main/java/com/haa/webappspringsecurity/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.haa.webappspringsecurity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+    @GetMapping("/")
+    public String welcome(Model theModel) {
+        return "home";
+    }
+}
